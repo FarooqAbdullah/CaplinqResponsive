6179b704844520ff0a21b2ad5a93675af9321072
Jawad <jawad@webxity.com>
2015-05-25 06:23:35 -0700

    added sticky navigation

--- js/custom.js
+++ js/custom.js
@@ -8,5 +8,28 @@ jQuery(document).ready(
 		$('#search_hide').click(function(e) {
             e.stopPropagation();
         });
+	
+	
+				 var stickyNavTop = $('.main_navigation').offset().top; 
+				 
+				var stickyNav = function(){
+				var scrollTop = $(window).scrollTop();
+				  
+				if (scrollTop > stickyNavTop) { 
+					$('.main_navigation').addClass('sticky');
+				} else {
+					$('.main_navigation').removeClass('sticky'); 
+				}
+				};
+				 
+				stickyNav();
+				 
+				$(window).scroll(function() {
+					stickyNav();
+					});
 	}
+	
 );
+			
+
+              
